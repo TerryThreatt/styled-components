56857b91625b2f6434c4eb3fa8ac62dabb1aa5fa
TerryThreatt <terry.threatt@gmail.com>
2021-05-15 10:17:10 -0500

    add new theme

---
+++ src/utils/themes.js
@@ -0,0 +1,13 @@
+import { green, neutral } from './colors';
+import { primaryFont } from './typography'
+
+
+export const defaultTheme = {
+    primaryColor: green[100],
+    primaryColorHover: green[200],
+    primaryColorActive: green[300],
+    textColorOnPrimary: neutral[100],
+    textColor: neutral[600],
+    primaryFont
+}
+
